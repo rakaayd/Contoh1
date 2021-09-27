e6de89cb044d95612d810c2b93b81916cd18943c
rakaayd <91060158+rakaayd@users.noreply.github.com>
2021-09-27 15:09:52 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller {
+
+    public function index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo "nama saya lionel messi saya tinggal di paris kesukaan saya bermain sepak bola";
+    }
+
+}
